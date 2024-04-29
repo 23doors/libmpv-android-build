2641a81451c6dab1d8bf746c5aa08043552a3cdc
Robert Kopaczewski <rk@23doors.com>
2024-04-29 09:57:19 +0200

    mpv vulkan

--- buildscripts/scripts/mpv.sh
+++ buildscripts/scripts/mpv.sh
@@ -14,6 +14,16 @@ else
 	exit 255
 fi
 
+# Android provides Vulkan, but no pkgconfig file
+# you can double-check the version in vk.xml (ctrl+f VK_API_VERSION)
+mkdir -p "$prefix_dir"/lib/pkgconfig
+cat >"$prefix_dir"/lib/pkgconfig/vulkan.pc <<"END"
+Name: Vulkan
+Description:
+Version: 1.2.0
+Libs: -lvulkan
+Cflags:
+END
 unset CC CXX # meson wants these unset
 
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
